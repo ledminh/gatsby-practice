a11dca9bf70d9fd0441aee8dd5bd06169b1b465f
Minh Le <duyminh.le310@gmail.com>
2022-05-20 14:23:45 -0400

    create pages

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function AboutPage() {
+
+
+    return (
+        <main>
+            <h1>About this site!!!!</h1>
+        </main>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function IndexPage() {
+
+
+    return (
+        <main>
+            <h1>Hello Frontend Masters!</h1>
+        </main>
+    )
+}
